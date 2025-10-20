a2038dd960f580b8e23404e978d54cc8f0b137cc
musashaheen2620-cpu <musashaheen2620@gmail.com>
2025-10-20 23:59:27 +0200

    Order.cs

--- Order.cs
+++ Order.cs
@@ -7,6 +7,6 @@ namespace InventorySystem
         public int OrderId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        public string Status { get; set; }  
+        public string Status { get; set; }  - // "Queued" or "Processed"
     }
 }
